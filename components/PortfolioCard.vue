cc80bb193736cd6113d132ded1ee0faa0b366740
Felipe Flor <f.flor@vehikl.com>
2019-03-31 17:42:21 -0400

    Bring the Portfolio page from last website version

---
+++ components/PortfolioCard.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="project-card elevation-3" :style="$vuetify.breakpoint.smAndUp ? desktopGridTemplateArea : ''">
+        <v-hover>
+            <img class="project-image"
+                 slot-scope="{ hover }"
+                 :class="{'elevation-5 bigger' : hover}"
+                 :alt="data.imageAlt"
+                 :src="data.imageSrc"
+                 @click="window.open(imageDestination, '_blank')"
+            >
+        </v-hover>
+
+        <h2 class='project-title headline font-weight-thin' v-text="data.title"></h2>
+
+        <div class="project-description" v-html="data.description">
+        </div>
+
+        <v-layout class="keywords-container" row wrap align-center justify-center>
+            <v-chip outline color="primary" v-for="(keyword, i) in data.keywords" :key="i">{{keyword}}</v-chip>
+        </v-layout>
+
+        <v-layout class="links-container" row wrap align-center justify-center>
+            <v-btn class="primary" v-if="data.website" :href="data.website" target="_blank" small>Visit Website</v-btn>
+            <v-btn class="secondary" v-if="data.source" :href="data.source" target="_blank" small>See Source Code
+            </v-btn>
+        </v-layout>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'nuxt-property-decorator';
+
+    export interface PortfolioCardData {
+        imageSrc: string;
+        imageAlt: string;
+        title: string;
+        description: string;
+        keywords: string[];
+        website?: string;
+        source?: string;
+    }
+
+    @Component
+    export default class PortfolioCard extends Vue {
+        @Prop() private data!: PortfolioCardData;
+
+        protected desktopGridTemplateArea = {
+            'grid-template-areas': `"image title"
+                                    "image description"
+                                    "image skills"
+                                    ". links"`,
+        };
+
+        get window() {
+            return window;
+        }
+
+        get imageDestination() {
+            return this.data.website ? this.data.website : this.data.source;
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .project-card {
+        display: grid;
+        grid-gap: 20px;
+        grid-template-areas: "title" "image" "description" "skills" "links";
+        border: cadetblue 1px solid;
+        border-radius: 20px;
+        padding: 20px;
+        max-width: 45rem;
+
+        .project-image {
+            transition: all 350ms ease;
+            border: gainsboro 1px solid;
+
+            &:hover {
+                cursor: pointer;
+            }
+
+            &.bigger {
+                transform: scale(1.025);
+            }
+
+            height: 200px;
+            max-width: 225px;
+            grid-area: image;
+            justify-self: center;
+            align-self: center;
+        }
+
+        .project-title {
+            grid-area: title;
+            justify-self: center;
+        }
+
+        .project-description {
+            grid-area: description;
+            justify-self: center;
+        }
+
+        .keywords-container {
+            grid-area: skills;
+            justify-self: center;
+        }
+
+        .links-container {
+            grid-area: links;
+            justify-self: center;
+        }
+    }
+</style>
---
+++ pages/portfolio/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class='portfolio'>
+    <v-container>
+      <v-layout row wrap justify-center>
+        <portfolio-card :data='catMousePorfolio' :class="$vuetify.breakpoint.xsOnly ? 'my-5' : 'ma-5'"/>
+        <portfolio-card :data='balloonStory' :class="$vuetify.breakpoint.xsOnly ? 'my-5' : 'ma-5'"/>
+        <portfolio-card :data='musicGame' :class="$vuetify.breakpoint.xsOnly ? 'my-5' : 'ma-5'"/>
+        <portfolio-card :data='felipeFlorDotCom' :class="$vuetify.breakpoint.xsOnly ? 'my-5' : 'ma-5'"/>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script lang='ts'>
+  import {Component, Vue} from 'nuxt-property-decorator';
+  import PortfolioCard, {PortfolioCardData} from '~/components/PortfolioCard.vue';
+
+  @Component({components: {PortfolioCard}})
+  export default class PortfolioPage extends Vue {
+    protected catMousePorfolio: PortfolioCardData = {
+      imageSrc: 'https://camo.githubusercontent.com/cab2013dfd5c75db71fedba72738e41597d848c6/68747470733a2f2f7' +
+              '265732e636c6f7564696e6172792e636f6d2f66656c697065666c6f722f696d6167652f75706c6f61642f635f7363616c65' +
+              '2c685f3435302f76313535323732303438372f312e6a7067',
+      imageAlt: 'Screencap of Cat & Mouse app',
+      title: 'Cat Mouse',
+      description: `
+               This project started because I wanted to learn Graph Theory and Path Finding Algorithms. </br></br>
+            It demonstrates the <strong class="blue--text text--darken-2">Traveling Salesman Problem.</strong></br></br>
+            This app allows you to create a maze with some rewards for the cat (milk and mouse), the cat
+            will calculate the optimal path to get the milk and the mouse.
+            `,
+      keywords: ['Breadth First Search', 'Graph Theory', 'Vue', 'Typescript', 'Mocha', 'Vue Test Utils'],
+      website: 'https://catmouse.felipeflor.com/',
+      source: 'https://github.com/FRFlor/cat-mouse',
+    };
+
+    protected balloonStory: PortfolioCardData = {
+      imageSrc: 'https://res.cloudinary.com/felipeflor/image/upload/v1553831891/Balloon%20story.png',
+      imageAlt: 'Art of girl catching a balloon',
+      title: 'Balloon Story',
+      description: `
+               Animation that <strong class="blue--text text--darken-2"> won 1st place</strong>
+               in the Art Submission Contest of VueConf US 2019! </br></br>
+               Background and object movement are randomly generated; the app also demonstrates collision physics
+               between the snow flakes and the balloon. Everything was made from scratch using canvas.
+            `,
+      keywords: ['Vue', 'Javascript', 'VueConf 2019', '1st Place Winner'],
+      website: '',
+      source: 'https://codepen.io/frflor/pen/QoRmRq',
+    };
+
+    protected musicGame: PortfolioCardData = {
+      imageSrc: 'https://res.cloudinary.com/felipeflor/image/upload/v1553845714/The%20MusicGame.png',
+      imageAlt: 'Screenshot of webapp showing ariel (Little Mermaid) singing \"Part of Your World\"',
+      title: 'Disney Music Game!',
+      description: `
+               I'm a big fan of Disney Songs in general, so this project had to happen eventually! </br></br>
+               Can you guess the song? This WebApp will randomly select a certain Disney song and, by using
+               YouTube's API, play random parts of that song at random speeds (super slow or super fast).</br></br>
+               You have a few seconds to guess, once the time is up, the answer is revealed with a video playing
+               an iconic part of that song.
+            `,
+      keywords: ['Vue', 'Vuetify', 'Youtube', 'Laravel', 'MySql'],
+      website: 'https://musicgame.felipeflor.com/',
+      source: 'https://github.com/FRFlor/music-game',
+    };
+
+    protected felipeFlorDotCom: PortfolioCardData = {
+      imageSrc: 'https://res.cloudinary.com/felipeflor/image/upload/v1553837166/recursion_meme.png',
+      imageAlt: 'Meme about recursion',
+      title: 'FelipeFlor.com',
+      description: `
+               My personal website. You're looking at it right now!</br></br>
+               It was made with Vue using Vuetify components. The server was prepared by me,
+               by installing the required resources for a typical LEMP stack server;
+               SSL was achieved with Let's Encrypt (free SSL).
+            `,
+      keywords: ['Vue', 'Vuetify', 'Typescript'],
+      website: 'https://www.felipeflor.com/',
+      source: 'https://github.com/FRFlor/felipeflor.com',
+    };
+  }
+</script>
+
