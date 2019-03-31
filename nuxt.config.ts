3c04d64894edb431d3150b82229a96a5bacc500f
Felipe Flor <f.flor@vehikl.com>
2019-03-31 17:30:32 -0400

    Use cdn for google fonts and material icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,11 @@ export default {
         meta: [
             {charset: "utf-8"},
             {name: "viewport", content: "width=device-width, initial-scale=1"},
-            {hid: "description", name: "description", content: "Felipe Flor - WebDeveloper - Experience with Vue, Laravel and Typescript"}
+            {
+                hid: "description",
+                name: "description",
+                content: "Felipe Flor - WebDeveloper - Experience with Vue, Laravel and Typescript"
+            }
         ],
         link: [
             {rel: "icon", type: "image/x-icon", href: "/favicon.ico"},
@@ -14,6 +18,14 @@ export default {
                 href: "https://use.fontawesome.com/releases/v5.8.1/css/all.css",
                 integrity: "sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf",
                 crossorigin: "anonymous"
+            },
+            {
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css?family=Material+Icons"
+            },
+            {
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900"
             }
         ]
     },
