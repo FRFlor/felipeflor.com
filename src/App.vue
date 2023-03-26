8d071282015ed412d3f3f3d87facc9032d9e7d2a
Felipe Flor <f.flor@vehikl.com>
2023-03-26 03:28:54 -0400

    tweak styling

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,21 @@ import ProjectCard from "@/components/ProjectCard.vue"</script>
     </p>
     <h2 class="mt-14 lg:mt-24">Check out some of my side projects!</h2>
     <ul class="mt-6 lg:mt-14 flex justify-around xl:justify-between gap-y-6 flex-wrap">
+      <li>
+        <project-card source-code-url="https://github.com/FRFlor/cat-mouse"
+                      title="Cat Mouse"
+                      video-poster="https://res.cloudinary.com/felipeflor/video/upload/v1556653952/felipeflor.com/catmouse.jpg"
+                      video-source="https://res.cloudinary.com/felipeflor/video/upload/v1556653952/felipeflor.com/catmouse.webm"
+                      website-url="https://catmouse.felipeflor.com/"
+        >
+          <p>This project started because I wanted to learn Graph Theory and Path Finding Algorithms.</p>
+          <p>It demonstrates the <strong>Traveling Salesman Problem</strong>.</p>
+          <p>
+            This app allows you to create a maze with some rewards for the cat (milk and mouse), the cat
+            will calculate the optimal path to get the milk and the mouse.
+          </p>
+        </project-card>
+      </li>
       <li v-for="i in 8">
         <project-card title="Balloon Story"
                       video-poster="https://res.cloudinary.com/felipeflor/video/upload/v1556653952/felipeflor.com/balloonStory.jpg"
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -12,7 +12,7 @@ defineProps<{
 
 <template>
   <div
-      class="project-card w-full md:w-96 2xl:w-120 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      class="project-card overflow-hidden w-full md:w-96 2xl:w-120 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <video
         :poster="videoPoster"
         autoplay
@@ -26,30 +26,32 @@ defineProps<{
           type="video/webm"
       >
     </video>
-    <div class="p-5">
+    <div class="p-5 h-96">
       <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-blue-100">{{ title }}</h3>
       <slot><p>This is a placeholder for the description of the project</p></slot>
 
-      <section class="mb-6">
-        <v-chip text="Vue"/>
-        <v-chip text="Javascript"/>
-        <v-chip text="VueConf 2019"/>
-      </section>
+      <div class="flex flex-col h-full justify-between">
+        <section>
+          <v-chip text="Vue"/>
+          <v-chip text="Javascript"/>
+          <v-chip text="VueConf 2019"/>
+        </section>
 
-      <section class="gap-4 flex">
-        <a :href="websiteUrl"
-           v-if="websiteUrl"
-           data-test="visit-website"
-           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Visit website
-        </a>
-        <a v-if="sourceCodeUrl"
-           :href="sourceCodeUrl"
-           class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-           data-test="visit-source-code">
-          See source code
-        </a>
-      </section>
+        <section class="gap-4 flex">
+          <a v-if="websiteUrl"
+             :href="websiteUrl"
+             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+             data-test="visit-website">
+            Visit website
+          </a>
+          <a v-if="sourceCodeUrl"
+             :href="sourceCodeUrl"
+             class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+             data-test="visit-source-code">
+            See source code
+          </a>
+        </section>
+      </div>
     </div>
   </div>
 </template>
