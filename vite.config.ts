9aedf51f552d32a5e7a2875065cfefe619222332
Felipe Flor <f.flor@vehikl.com>
2023-04-23 14:05:52 -0400

    Add typechecking vie vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import vue from "@vitejs/plugin-vue"
 import ssr from "vite-plugin-ssr/plugin"
 import {defineConfig} from "vitest/config"
 import path from "path"
+import checker from "vite-plugin-checker"
 
 export default defineConfig({
-    plugins: [vue(), ssr()],
+    plugins: [vue(), ssr(), checker({vueTsc: true})],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src")
